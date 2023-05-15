fe09c0c5924f83c2c0f40bd0b208a31d9a1a7c7d
raselmahmud22 <raselmahmud454b@gmail.com>
2023-05-15 21:19:46 +0800

    reservetion section added

--- src/components/HomePageComponents/MakeReservation.jsx
+++ src/components/HomePageComponents/MakeReservation.jsx
@@ -16,25 +16,34 @@ const MakeReservation = () => {
         </h2>
       </div>
 
-      <div className="grid grid-cols-2 items-center my-20">
-        <p
-          className="text-xl lg:mr-9 text-gray-100">
-          Centuries of talented cooks and exceptional palettes have played a
-          role in forming what we recognize today as Middle Eastern cuisine.
-          <br />
-          <br />
-          When you enter our African Soul Food we want you to feel like you have
-          met new friends - a new family - who will guide you through our
-          region's most memorable offerings, we hope to transport you to country
-          sides of Iran, where we all sit around the table and wait to be blown
-          away by our mothers and grandmothers cooking while enjoying the
-          company of our favorite people.
-        </p>
-        <img
-          src="https://images.squarespace-cdn.com/content/v1/6434de7a3f788a47f32b88e8/303d6de6-a07c-4f7a-9de3-744057968861/Atlanta-middle-eastern-cuisine.jpg?format=750w"
-          alt=""
-          className="rounded-md lg:ml"
-        />
+      <div className="grid grid-cols-2 items-center my-28">
+        <div>
+          <p className="text-xl lg:mr-9 text-gray-100">
+            Centuries of talented cooks and exceptional palettes have played a
+            role in forming what we recognize today as Middle Eastern cuisine.
+            <br />
+            <br />
+            When you enter our African Soul Food we want you to feel like you
+            have met new friends - a new family - who will guide you through our
+            region's most memorable offerings, we hope to transport you to
+            country sides of Iran, where we all sit around the table and wait to
+            be blown away by our mothers and grandmothers cooking while enjoying
+            the company of our favorite people.
+          </p>
+          <button className="btn text-gray-100 btn-lg mt-8">MAKE A RESERVATION</button>
+        </div>
+        <div className="flex">
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/6434de7a3f788a47f32b88e8/53005c28-587d-4557-991f-cd73e417a241/Middle+Eastern+Seabass+vegetables.png?format=500w"
+            alt=""
+            className="rounded-md lg:ml max-w-sm max-h-96"
+          />
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/6434de7a3f788a47f32b88e8/6813ac3c-42e8-4480-adcc-20887753dba4/middle-eastern-kookoo-sabzi+-atlanta+copy.png?format=500w"
+            alt=""
+            className="rounded-md lg:ml max-w-sm max-h-96"
+          />
+        </div>
       </div>
     </>
   );
